b5310c6d129f8f9984a411645ec6dbaaee7ff2a3
arajitdev <arajitdebnathofficial@gmail.com>
2025-07-11 08:45:46 +0530

    feature implemented

--- src/Expense.jsx
+++ src/Expense.jsx
@@ -1,4 +1,40 @@
+import { useState } from "react"
+
 const Expense = ()=>{
+    const[transaction , setTransaction] = useState([])
+    const[title , setTitle] = useState("");
+    const[amount , setAmount] = useState();
+    const[date , setDate] = useState();
+    const[category , setCategory]  = useState();
+    
+    const addTransaction = ()=>{
+        if(title.trim!=""){
+            const objects = {
+                title: title,
+                amount: amount,
+                date: date,
+                category: category
+            }
+            setTransaction(t=>[...t ,objects])
+            
+        }
+        
+    }
+
+    const titleHandler = (e) =>{
+        setTitle(e.target.value);
+    }
+    const dateHandler = (e)=>{
+        setDate(e.target.value);
+    }
+    const amountHandler = (e)=>{
+        setAmount(e.target.value);
+    }
+    const categoryHandler = (e)=>{
+      setCategory(e.target.value)
+    }
+    
+
     return(
         <>
         <div>
@@ -7,11 +43,11 @@ const Expense = ()=>{
 
       <div className="main-container">
         <div className="expense-details">
-          <input className="title" type="text" placeholder="Enter expense details" />
-          <input className="amount" type="number" placeholder="Enter the amount" />
-          <input className="date" type="date" />
-            <select>
-              <option value="">Food</option>
+          <input onChange={(e)=>titleHandler(e)} className="title" type="text" placeholder="Enter expense details" />
+          <input onChange={(e)=>amountHandler(e)} className="amount" type="number" placeholder="Enter the amount" />
+          <input onChange={(e)=>dateHandler(e)} className="date" type="date" />
+            <select onChange={(e)=>categoryHandler(e)}>
+              <option value="Food">Food</option>
               <option value="Transportation">Transportation</option>
               <option value="Housing & rent">Housing & rent</option>
               <option value="Bills & Subscriptions">Bills & Subscriptions</option>
@@ -19,7 +55,7 @@ const Expense = ()=>{
               <option value="Travel">Travel</option>
               <option value="Education">Education</option>
             </select>
-            <button className="add-btn">
+            <button onClick={addTransaction} className="add-btn">
               ADD
             </button>
             
@@ -34,20 +70,50 @@ const Expense = ()=>{
           <h4>Delete</h4>
           </div>
         </div>
+        <ul>
+            {/* <div className="Results">
+                <li>
+                    <div id="title" >
+                        <p> Booked a hotel </p>
+                    </div>
 
-        <div className="Results">
-            <ul>
-                <li><p>
-            Booked hotel
-          </p>
-          <p>
+          <div id="amount">
+            <p>
             10$
-          </p>
-          <p>10 july , 2025</p>
-          <p>Travel</p></li>
-            </ul>
+            </p>
+          </div>
           
+          <div id="date">
+            <p>10 july , 2025</p>
+          </div>
+
+        <div id="category">
+            <p >Travel</p>
         </div>
+
+          </li>
+            </div> */}
+            {transaction.map((trxn)=> 
+              <div className="Results">
+                <li>
+                  <div id="title">
+                    <p>{trxn.title}</p>
+                  </div>
+                  <div id="amount">
+                    <p>{trxn.amount}</p>
+                  </div>
+                  <div id="date">
+                    <p>{trxn.date}</p>
+                  </div>
+                  <div id="category">
+                    <p>{trxn.category}</p>
+                  </div>
+                </li>
+              </div>
+            )}
+        </ul>
+
+        
       </div>
     </div>
         </>
